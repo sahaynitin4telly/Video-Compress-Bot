6481e96c32e6fe716e67926178d003e78616db70
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-10 09:56:18 +0530

    Update admin.py

--- bot/plugins/admin.py
+++ bot/plugins/admin.py
@@ -20,14 +20,14 @@ broadcast_ids = {}
 async def sts(c, m):
     
     total_users = await db.total_users_count()
-    await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
+    await m.reply_text(text=f"Total user(s) 🤠 {total_users}", quote=True)
 
 
 async def ban(c, m):
     
     if len(m.command) == 1:
         await m.reply_text(
-            f"Use this command to ban any user from the bot.\n\nUsage:\n\n`/ban_user user_id ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
+            f"Use this command to ban 🛑 any user from the bot 🤖.\n\nUsage:\n\n`/ban_user user_id ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
             quote=True
         )
         return
@@ -41,12 +41,12 @@ async def ban(c, m):
         try:
             await c.send_message(
                 user_id,
-                f"You are banned to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n**Message from the admin**"
+                f"You are Banned 🚫 to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n**Message from the admin 🤠**"
             )
             ban_log_text += '\n\nUser notified successfully!'
         except:
             traceback.print_exc()
-            ban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
+            ban_log_text += f"\n\n ⚠️ User notification failed! ⚠️ \n\n`{traceback.format_exc()}`"
         await db.ban_user(user_id, ban_duration, ban_reason)
         print(ban_log_text)
         await m.reply_text(
@@ -56,7 +56,7 @@ async def ban(c, m):
     except:
         traceback.print_exc()
         await m.reply_text(
-            f"Error occoured! Traceback given below\n\n`{traceback.format_exc()}`",
+            f"Error occoured ⚠️! Traceback given below\n\n`{traceback.format_exc()}`",
             quote=True
         )
 
@@ -64,24 +64,24 @@ async def ban(c, m):
 async def unban(c, m):
     if len(m.command) == 1:
         await m.reply_text(
-            f"Use this command to unban any user.\n\nUsage:\n\n`/unban_user user_id`\n\nEg: `/unban_user 1234567`\n This will unban user with id `1234567`.",
+            f"Use this command to unban 😃 any user.\n\nUsage:\n\n`/unban_user user_id`\n\nEg: `/unban_user 1234567`\n This will unban user with id `1234567`.",
             quote=True
         )
         return
     
     try:
         user_id = int(m.command[1])
-        unban_log_text = f"Unbanning user {user_id}"
+        unban_log_text = f"Unbanning user 🤪 {user_id}"
         
         try:
             await c.send_message(
                 user_id,
                 f"Your ban was lifted!"
             )
-            unban_log_text += '\n\nUser notified successfully!'
+            unban_log_text += '\n\n✅ User notified successfully! ✅'
         except:
             traceback.print_exc()
-            unban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
+            unban_log_text += f"\n\n⚠️ User notification failed! ⚠️\n\n`{traceback.format_exc()}`"
         await db.remove_ban(user_id)
         print(unban_log_text)
         await m.reply_text(
@@ -91,7 +91,7 @@ async def unban(c, m):
     except:
         traceback.print_exc()
         await m.reply_text(
-            f"Error occoured! Traceback given below\n\n`{traceback.format_exc()}`",
+            f"⚠️ Error occoured ⚠️! Traceback given below\n\n`{traceback.format_exc()}`",
             quote=True
         )
 
@@ -106,8 +106,8 @@ async def _banned_usrs(c, m):
         banned_on = banned_user['ban_status']['banned_on']
         ban_reason = banned_user['ban_status']['ban_reason']
         banned_usr_count += 1
-        text += f"> **user_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
-    reply_text = f"Total banned user(s): `{banned_usr_count}`\n\n{text}"
+        text += f"> **User_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
+    reply_text = f"Total banned user(s) 🤭: `{banned_usr_count}`\n\n{text}"
     if len(reply_text) > 4096:
         with open('banned-users.txt', 'w') as f:
             f.write(reply_text)
