830d5513f5e1c11bae6a8f1f2b8a28acd632e7ba
Mr. Developer <77911154+MrBotDeveloper@users.noreply.github.com>
2021-02-19 11:11:28 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -206,7 +206,7 @@ async def incoming_compress_message_f(bot, update):
     bst_now = utc_now + datetime.timedelta(minutes=00, hours=6)
     bst = bst_now.strftime("%d/%m/%Y, %H:%M:%S")
     now = f"\n{ist} (GMT+05:30)`\n`{bst} (GMT+06:00)"
-    download_start = await bot.send_message(chat_id, f"**Bot Become Busy Now !!**Used Percentage `{target_percentage}`℅\n, parse_mode="markdown")
+    download_start = await bot.send_message(chat_id, f"**Bot Become Busy Now !!**\n Used Percentage `{target_percentage}` \n\nDownload Started at `{now}`", parse_mode="markdown")
     try:
       d_start = time.time()
       status = DOWNLOAD_LOCATION + "/status.json"
